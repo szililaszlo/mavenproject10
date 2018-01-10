0742afea830ec05e7c6377106ce2b93d390b94a6
szililaszlo <laszlo0919@gmail.com>
2018-01-10 17:30:22 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+    }
+
+}
---
+++ src/main/java/Person.java
@@ -0,0 +1,40 @@
+public class Person {
+
+    private String name;
+    private String role;
+    private String city;
+
+    public Person() {
+
+    }
+
+    public Person(String name, String role, String city) {
+        this.name = name;
+        this.role = role;
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
